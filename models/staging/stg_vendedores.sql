fab4dd986f206d47a4b72724ab89c120484e04d0
Rafael dos Santos <rahfaeu@gmail.com>
2024-06-13 12:47:05 +0000

    :construction: criação das dimensões

--- models/staging/stg_vendedores.sql
+++ models/staging/stg_vendedores.sql
@@ -3,7 +3,8 @@
 with source as (
     select
         cast(id_vendedores as int64) as vendedor_id
-        , initcap(cast(nome as string)) as nome_vendedor
+        -- , initcap(cast(nome as string)) as vendedor_nome
+        , {{ dbt_utils.generate_surrogate_key(['nome']) }} as vendedor_nome
         , cast(id_concessionarias as int64) as concessionaria_id
         , timestamp_trunc(data_inclusao, minute) as data_inclusao
         , timestamp_trunc(coalesce(data_atualizacao, data_inclusao), minute) as data_atualizacao
